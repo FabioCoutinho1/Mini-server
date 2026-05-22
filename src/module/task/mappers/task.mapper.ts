51815d2686aef784cf16bc2cec82894537887d72
FabioCoutinho <fabiofarias549@gmail.com>
2026-05-22 16:54:59 -0300

    created new modulo for task

---
+++ src/module/task/mappers/task.mapper.ts
@@ -0,0 +1,8 @@
+import type { tasks } from "../../../../generated/prisma/index.js";
+import { Task } from "../entities/task.entity.js";
+
+export class MapperTask {
+  static toEntity(task: tasks): Task {
+    return new Task(task.id, task.name, task.done, task.favorite);
+  }
+}
